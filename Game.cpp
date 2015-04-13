6feb49ee518b269447142283a1c9a4b43be90db2
Zoltán Sasvári <z.sasvari@student.tugraz.at>
2015-04-13 20:51:34 +0200

    implemented basic command parsing

--- Game.cpp
+++ Game.cpp
@@ -8,9 +8,46 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 #include "Game.h"
 
 using namespace std;
+
+enum cmd : char{
+  CMD_QUIT = 0,
+  CMD_ADDTILE = 1,
+  CMD_WRITE = 2,
+  CMD_ERROR = 3,
+  CMD_BLANK = 4,
+};
+
+struct args{
+  cmd command;
+  string *arg[5];
+};
+
+void getCMD(string input, args &arguments){
+  if(input == ""){
+    arguments.command = CMD_BLANK;
+  }
+  stringstream iss(input);
+  string buf;
+  for (char i = 0; iss >> buf; i++) {
+    arguments.arg[i] = new string(buf);
+  }
+  if (*arguments.arg[0] == "AddTile") {
+    arguments.command = CMD_ADDTILE;
+  }
+  else if (*arguments.arg[0] == "write"){
+    arguments.command = CMD_WRITE;
+  }
+  else if (*arguments.arg[0] == "quit"){
+    arguments.command = CMD_QUIT;
+  }
+  else {
+    arguments.command = CMD_ERROR;
+  }
+}
 
 Game::Game(){
   Activeplayer = COLOR_WHITE;
@@ -25,12 +62,28 @@ void Game::setStarttile(Tile *input){
 void Game::run(){
   Running = true;
   string input;
+  struct args arguments;
   while (Running == true) {
     cout << "sep> ";
-    cin >> input;
-    if (input == "quit") {
-      Running = false;
-      return;
+    //cin >> input;
+    getline(cin, input);
+    getCMD(input, arguments);
+
+    switch (arguments.command) {
+      case CMD_BLANK:
+        cout << endl;
+        break;
+      case CMD_QUIT:
+        return;
+      case CMD_ADDTILE:
+        cout << "Addtile" << endl;
+        break;
+      case CMD_WRITE:
+        cout << "write" << endl;
+        break;
+      case CMD_ERROR:
+        cout << "Error: Unknown command!" << endl;
+        break;
     }
   }
 }
--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 {
   std::string *filename;
   //chack for arguments
-  if (strcmp(argv[1], "-g")) {
+  if (argc > 1 && !strcmp(argv[1], "-g")) {
     if (argv[2] == nullptr) {
       //error
     } else {
